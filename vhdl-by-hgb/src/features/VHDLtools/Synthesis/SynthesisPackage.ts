36b44bcf29a51da382fcd54a18520479ee60b0ab
Jakob Jungreuthmayer <s2110306009@fhooe.at>
2023-05-30 10:35:42 +0200

    new enum + corresponding map

--- vhdl-by-hgb/src/features/VHDLtools/Synthesis/SynthesisPackage.ts
+++ vhdl-by-hgb/src/features/VHDLtools/Synthesis/SynthesisPackage.ts
@@ -5,11 +5,21 @@ import { ISynthesisFactory } from "./Factory/SynthesisFactory";
 export enum eSynthesisTool {
     Diamond = "Diamond",
     Quartus = "Quartus",
-    Synopsis = "Synopsis",
     Vivado = "Vivado"
+}
+
+export enum eSynthesisProjectFile {
+    Diamond = "ldf",
+    Quartus = "qsf",
+    Vivado = "xpr"
 }
 
 // mapping strings of eSynthesisTool-Enum to their factories
 export const SynthesisToolMap : Map<string,ISynthesisFactory> = new Map<string,ISynthesisFactory>([
-[eSynthesisTool.Quartus, QuartusFactory.getInstance()],
+    [eSynthesisTool.Quartus, QuartusFactory.getInstance()],
+]);
+
+// mapping strings of eSynthesisProjectFile-Enum to their factories
+export const SynthesisProjectFileMap : Map<string,ISynthesisFactory> = new Map<string,ISynthesisFactory>([
+    [eSynthesisProjectFile.Quartus, QuartusFactory.getInstance()],
 ]);
